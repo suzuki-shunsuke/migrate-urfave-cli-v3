96132071432d5d1147cdabd9c06c3e32bd0c1c79
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-01 19:28:49 +0900

    feat: add the script (#1)
    * feat: add the script
    
    * ci: set up
    
    * chore: configure Renovate

---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+patterns=(
+	's|"github\.com/urfave/cli/v2"|"github.com/urfave/cli/v3"|'
+	's|"github\.com/urfave/cli/v2/altsrc"|"github.com/urfave/cli-altsrc/v3"|'
+	's|cli\.App|cli.Command|'
+	's|EnableBashCompletion|EnableShellCompletion|'
+	's|RunContext|Run|'
+	's|Subcommands|Commands|'
+	's|\(([^ ]+?) \*cli\.Context\) error|(ctx context.Context, \1 *cli.Command) error|'
+	's|\(\*cli\.Context\) error|(context.Context, *cli.Command) error|'
+	's|\*cli\.Context|context.Context|'
+	's|EnvVars: \[\]string|Sources: EnvVars|'
+)
+
+while read -r file; do
+	for p in "${patterns[@]}"; do
+		sed -Ei "$p" "$file"
+	done
+	sed -Ei "/^	*Compiled: /d" "$file"
+done < <(git ls-files | grep -E '\.go$')
+
+go mod tidy
