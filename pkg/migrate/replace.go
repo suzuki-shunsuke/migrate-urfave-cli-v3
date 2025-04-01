f5c6538fe15cc399c0e99dd01a661b1a2cbffdb4
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-02 06:38:44 +0900

    refactor: move code

--- pkg/migrate/replace.go
+++ pkg/migrate/replace.go
@@ -5,6 +5,31 @@ import (
 	"regexp"
 )
 
+func newReplacers() ([]*Replacer, error) {
+	replacers := []*Replacer{
+		newReplacer(`"github\.com/urfave/cli/v2"`, `"github.com/urfave/cli/v3"`),
+		newReplacer(`"github\.com/urfave/cli/v2/altsrc"`, `"github.com/urfave/cli-altsrc/v3"`),
+		newReplacer(`cli\.App`, `cli.Command`),
+		newReplacer(`EnableBashCompletion`, `EnableShellCompletion`),
+		newReplacer(`RunContext`, `Run`),
+		newReplacer(`Subcommands`, `Commands`),
+		newReplacer(`CustomAppHelpTemplate`, `CustomRootCommandHelpTemplate`),
+		newReplacer(`cli\.NewApp\(\)`, `&cli.Command{}`),
+		newReplacer(`\(([^ ]+?) \*cli\.Context\) error`, `(ctx context.Context, $1 *cli.Command) error`),
+		newReplacer(`\(\*cli\.Context\) error`, `(context.Context, *cli.Command) error`),
+		newReplacer(`ExitErrHandler = func\(\*cli\.Context, error\)`, `ExitErrHandler = func(context\.Context, *cli.Command, error)`),
+		newReplacer(`\*cli\.Context`, `context.Context`),
+		newReplacer(`EnvVars: \[\]string\{([^}]+)\}`, `Sources: cli.EnvVars($1)`),
+		newReplacer(`EnvVars: \[\]string`, `Sources: cli.EnvVars`),
+	}
+	for _, r := range replacers {
+		if err := r.Init(); err != nil {
+			return nil, fmt.Errorf("init replacer: %w", err)
+		}
+	}
+	return replacers, nil
+}
+
 type Replacer struct {
 	pattern     string
 	regexp      *regexp.Regexp
@@ -29,29 +54,4 @@ func newReplacer(pattern, replacement string) *Replacer {
 
 func (r *Replacer) Replace(input []byte) []byte {
 	return r.regexp.ReplaceAll(input, r.replacement)
-}
-
-func newReplacers() ([]*Replacer, error) {
-	replacers := []*Replacer{
-		newReplacer(`"github\.com/urfave/cli/v2"`, `"github.com/urfave/cli/v3"`),
-		newReplacer(`"github\.com/urfave/cli/v2/altsrc"`, `"github.com/urfave/cli-altsrc/v3"`),
-		newReplacer(`cli\.App`, `cli.Command`),
-		newReplacer(`EnableBashCompletion`, `EnableShellCompletion`),
-		newReplacer(`RunContext`, `Run`),
-		newReplacer(`Subcommands`, `Commands`),
-		newReplacer(`CustomAppHelpTemplate`, `CustomRootCommandHelpTemplate`),
-		newReplacer(`cli\.NewApp\(\)`, `&cli.Command{}`),
-		newReplacer(`\(([^ ]+?) \*cli\.Context\) error`, `(ctx context.Context, $1 *cli.Command) error`),
-		newReplacer(`\(\*cli\.Context\) error`, `(context.Context, *cli.Command) error`),
-		newReplacer(`ExitErrHandler = func\(\*cli\.Context, error\)`, `ExitErrHandler = func(context\.Context, *cli.Command, error)`),
-		newReplacer(`\*cli\.Context`, `context.Context`),
-		newReplacer(`EnvVars: \[\]string\{([^}]+)\}`, `Sources: cli.EnvVars($1)`),
-		newReplacer(`EnvVars: \[\]string`, `Sources: cli.EnvVars`),
-	}
-	for _, r := range replacers {
-		if err := r.Init(); err != nil {
-			return nil, fmt.Errorf("init replacer: %w", err)
-		}
-	}
-	return replacers, nil
 }
