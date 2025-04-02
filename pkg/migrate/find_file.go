9f6c6c0b1f22ea02c12074226068940a8e26e085
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-02 09:21:56 +0900

    fix: output stderr of git ls-files (#19)

--- pkg/migrate/find_file.go
+++ pkg/migrate/find_file.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -12,6 +13,7 @@ func listFiles(ctx context.Context) ([]string, error) {
 	cmd := exec.CommandContext(ctx, "git", "ls-files")
 	buf := &bytes.Buffer{}
 	cmd.Stdout = buf
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		return nil, fmt.Errorf("git ls-files: %w", err)
 	}
