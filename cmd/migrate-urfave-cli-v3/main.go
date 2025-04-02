604b1ead501d38e321214d58701608bb4a66c37c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-04-02 09:07:16 +0900

    feat: support --help and --version options

--- cmd/migrate-urfave-cli-v3/main.go
+++ cmd/migrate-urfave-cli-v3/main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/suzuki-shunsuke/migrate-urfave-cli-v3/pkg/migrate"
 )
 
+var (
+	version = ""
+	commit  = "" //nolint:gochecknoglobals
+)
+
 func main() {
 	if err := run(os.Args); err != nil {
 		log.Fatal(err)
@@ -18,5 +23,11 @@ func main() {
 func run(args []string) error {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
-	return migrate.Run(ctx, args) //nolint:wrapcheck
+	runner := &migrate.Runner{
+		LDFlags: &migrate.LDFlags{
+			Version: version,
+			Commit:  commit,
+		},
+	}
+	return runner.Run(ctx, args) //nolint:wrapcheck
 }
--- pkg/migrate/run.go
+++ pkg/migrate/run.go
@@ -4,14 +4,36 @@ import (
 	"context"
 	"fmt"
 	"strings"
+
+	"github.com/urfave/cli/v3"
 )
 
-func Run(ctx context.Context, args []string) error {
+type Runner struct {
+	LDFlags *LDFlags
+}
+
+type LDFlags struct {
+	Version string
+	Commit  string
+}
+
+func (r *Runner) Run(ctx context.Context, args []string) error {
+	cmd := &cli.Command{
+		Name:        "migrate-urfave-cli-v3",
+		Usage:       "Migrate urfave/cli v2 to v3",
+		Description: "Migrate urfave/cli v2 to v3",
+		Version:     r.LDFlags.Version + " (" + r.LDFlags.Commit + ")",
+		Action:      r.action,
+	}
+	return cmd.Run(ctx, args) //nolint:wrapcheck
+}
+
+func (r *Runner) action(ctx context.Context, cmd *cli.Command) error {
 	replacers, err := newReplacers()
 	if err != nil {
 		return fmt.Errorf("new replacers: %w", err)
 	}
-	files := args[1:]
+	files := cmd.Args().Slice()
 	if len(files) == 0 {
 		a, err := listFiles(ctx)
 		if err != nil {
